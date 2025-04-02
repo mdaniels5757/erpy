974dff49e109d838685a064ce15eb01674ca0c88
Michael Daniels <michael.daniels618738@tufts.edu>
2025-04-02 15:33:10 -0400

    make compatible with nouse_stdio (alpha for now)

--- src/erpy/__init__.py
+++ src/erpy/__init__.py
@@ -3,24 +3,26 @@ from term.basetypes import Term
 from typing import Generator
 
 import struct
-import sys
+# import sys
 
 
 def _mailbox_gen() -> Generator[Term, None, None]:
+    input_stream = open(3, "rb")
     while True:
-        len_bin = sys.stdin.buffer.read(4)
+        len_bin = input_stream.read(4)
         if len(len_bin) != 4:
             return None
         (length,) = struct.unpack("!I", len_bin)
-        (term, rest) = codec.decode(sys.stdin.buffer.read(length))
+        (term, rest) = codec.decode(input_stream.read(length))
         yield term
 
 
 def _port_gen() -> Generator[None, Term, None]:
+    output_stream = open(4, "wb")
     while True:
         term = codec.encode((yield))
-        sys.stdout.buffer.write(struct.pack("!I", len(term)))
-        sys.stdout.buffer.write(term)
+        output_stream.write(struct.pack("!I", len(term)))
+        output_stream.write(term)
 
 
 def stdio_port_connection() -> (
